cd55ac4c9a34a39f9a9b99b0caf9769dedf388a5
Rodrigo Muinhos <rodrigomuinhostattooist@gmail.com>
2025-10-27 13:54:44 -0300

    trigger prod redeploy2

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,5 +1,18 @@
 import { startApp } from './app/main';
 
+if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations?.().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+    if (typeof caches !== 'undefined') {
+      caches.keys?.().then((keys) => {
+        keys.forEach((key) => {
+          void caches.delete(key);
+        });
+      });
+    }
+  });
+}
+
 startApp().catch((error) => {
   // eslint-disable-next-line no-console
   console.error('Failed to start Agenda Amiga', error);
